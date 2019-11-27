ff5b52f8071ed8e22f7abd8b5fb25fe7b8d9f859
Qiong Zeng <xinyufeifei@gmail.com>
2019-11-26 21:57:27 -0600

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -17,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         self.assertIn("To-Do", self.browser.title)
         header_text = self.browser.find_element_by_tag_name("h1").text
@@ -33,7 +33,7 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         inputbox = self.browser.find_element_by_id("id_new_item")
-        inputbox.send_keys("User peacock feathers to make a fly")
+        inputbox.send_keys("Buy peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
@@ -41,7 +41,3 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table("2: Buy peacock feathers to make a fly")
 
         self.fail("Finish the test!")
-
-
-if __name__ == "__main__":
-    unittest.main(warnings="ignore")
