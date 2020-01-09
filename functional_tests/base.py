05707ac4d6d3580af7bbc0e1660725bd2442a797
Qiong Zeng <xinyufeifei@gmail.com>
2020-01-09 10:19:29 -0600

    add functional test for my list.

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -35,6 +35,13 @@ class FunctionalTest(StaticLiveServerTestCase):
 
         return modified_fn
 
+    def add_list_item(self, item_text):
+        num_rows = len(self.browser.find_elements_by_css_selector("#id_list_table tr"))
+        self.get_item_input_box().send_keys(item_text)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        item_number = num_rows + 1
+        self.wait_for_row_in_list_table(f"{item_number}: {item_text}")
+
     @wait
     def wait_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id("id_list_table")
