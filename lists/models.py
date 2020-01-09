131d14bea87fa53aeb2589aca1c6c4b52cc65eaf
Qiong Zeng <xinyufeifei@gmail.com>
2020-01-09 13:49:38 -0600

    lists can have ownders, which are saved on creation

--- lists/models.py
+++ lists/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.urls import reverse
+from django.conf import settings
 
 
 # Create your models here.rm lis
 class List(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.PROTECT
+    )
+
     def get_absolute_url(self):
         return reverse("view_list", args=[self.id])
 
--- lists/views.py
+++ lists/views.py
@@ -27,8 +27,9 @@ def view_list(request, list_id):
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
-        list_ = List.objects.create()
-        list_.owner = request.user
+        list_ = List()
+        if request.user.is_authenticated:
+            list_.owner = request.user
         list_.save()
         form.save(for_list=list_)
         return redirect(list_)
